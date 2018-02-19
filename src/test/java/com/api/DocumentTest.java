62336f61ed522342a70ae57f5c9e2cb94cd251b3
patriciopo <patricio.poratto@gmail.com>
2018-02-19 16:48:51 -0300

    Add test to measure /content endpoint response time.

--- src/test/java/com/api/DocumentTest.java
+++ src/test/java/com/api/DocumentTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.Matchers.*;
 
 public class DocumentTest extends BaseTest{
 
-  @Test(groups = "Document.smoke")
+  @Test(groups = "Document.smoke", description = "Create a random document as the QE user.")
   public void testCreateDocument(){
     DocumentData doc = new DocumentData("Test_document_" + RandomStringUtils.randomAlphabetic(4));
     ValidatableResponse response =
@@ -95,5 +95,40 @@ public class DocumentTest extends BaseTest{
         .then()
         .statusCode(200)
         .body("subject", equalTo(doc.subject));
+  }
+
+  @Test(groups = "Document.smoke", description = "Expect response time to be under 2 seconds")
+  public void testDocumentResponseTime(){
+    Long timeout = 2000L;
+    String subject = "Test_document_" + RandomStringUtils.randomAlphabetic(4);
+    DocumentData doc = new DocumentData(subject);
+
+    //POST
+    String contentURI =
+        given()
+            .body(doc)
+            .post(API_CONTENT_URI)
+            .then()
+            .time(lessThan(timeout))
+            .extract().path("resources.self.ref");
+
+    //GET
+    given()
+        .get(contentURI)
+        .then()
+        .time(lessThan(timeout));
+
+    //PUT
+    given()
+        .body(doc)
+        .put(contentURI)
+        .then()
+        .time(lessThan(timeout));
+
+    //DELETE
+    given()
+        .delete(contentURI)
+        .then()
+        .time(lessThan(timeout));
   }
 }
